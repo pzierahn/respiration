5164d5a8c48fcc7a43f747fb938a1758aaf7119a
patrick <patrick.zierahn@student.kit.edu>
2024-04-14 13:58:34 +0200

    Added JSON utils

--- respiratory_extraction/utils/__init__.py
+++ respiratory_extraction/utils/__init__.py
@@ -1,3 +1,4 @@
 from .read_video import *
 from .unisens import *
 from .dataset import *
+from .json import *
---
+++ respiratory_extraction/utils/json.py
@@ -0,0 +1,10 @@
+import json
+
+
+def write_json(filename: str, data: any):
+    with open(filename, 'w') as file:
+        json.dump(data,
+                  file,
+                  indent=2,
+                  sort_keys=True,
+                  default=str)
