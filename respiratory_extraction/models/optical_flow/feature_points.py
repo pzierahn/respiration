bb8a6873349da3c6773ad5779a4ee2d507367163
patrick <patrick.zierahn@student.kit.edu>
2024-04-16 12:46:23 +0200

    Default to None mask

--- respiratory_extraction/models/optical_flow/feature_points.py
+++ respiratory_extraction/models/optical_flow/feature_points.py
@@ -6,7 +6,7 @@ def feature_point_selection(
         frame: np.ndarray,
         quality_level=float(0.3),
         quality_level_rv=float(0.05),
-        mask=np.ndarray | None,
+        mask=None | np.ndarray,
         max_corners=int(100),
         min_distance=int(7),
 ) -> np.ndarray:
@@ -41,7 +41,7 @@ def special_feature_point_selection(
         frame: np.ndarray,
         quality_level=float(0.3),
         quality_level_rv=float(0.05),
-        mask=np.ndarray | None,
+        mask=None | np.ndarray,
         max_corners=int(100),
         min_distance=int(7),
         fpn=int(5),
