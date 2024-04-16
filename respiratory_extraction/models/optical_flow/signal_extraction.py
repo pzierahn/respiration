4e4d118e803595b1e57037a45a56fb0e728d07ad
patrick <patrick.zierahn@student.kit.edu>
2024-04-16 14:06:03 +0200

    Refactor code

--- respiratory_extraction/models/optical_flow/signal_extraction.py
+++ respiratory_extraction/models/optical_flow/signal_extraction.py
@@ -3,18 +3,12 @@ from .feature_point_selection import *
 from .feature_point_movement import *
 
 
-def extract_respiratory_signal(
+def extract_raw_signal(
         frames: np.ndarray,
-        fps: int,
         fpn: None | int,
         roi_mask: None | np.ndarray = None,
         quality_level: float = 0.3,
         quality_level_rv: float = 0.05,
-        lowpass: float = 0.1,
-        highpass: float = 0.6,
-        use_cgof: bool = False,
-        use_filter: bool = False,
-        use_normalization: bool = False,
 ) -> np.ndarray:
     # 1. Extract the feature points
     feature_points = get_feature_points(
@@ -29,7 +23,18 @@ def extract_respiratory_signal(
     feature_point_movements = extract_feature_point_movement(frames, feature_points)
 
     # 3. Calculate the respiratory signal based on the feature point movements
-    point_amplitudes = np.sqrt(feature_point_movements[:, :, 0] ** 2 + feature_point_movements[:, :, 1] ** 2)
+    return np.sqrt(feature_point_movements[:, :, 0] ** 2 + feature_point_movements[:, :, 1] ** 2)
+
+
+def preprocess_signal(
+        point_amplitudes: np.ndarray,
+        fps: int,
+        lowpass: float = 0.1,
+        highpass: float = 0.6,
+        use_cgof: bool = False,
+        use_filter: bool = False,
+        use_normalization: bool = False,
+) -> np.ndarray:
     respiratory_signal = np.sum(point_amplitudes, 1) / point_amplitudes.shape[1]
 
     # Correlation-Guided Optical Flow Method
