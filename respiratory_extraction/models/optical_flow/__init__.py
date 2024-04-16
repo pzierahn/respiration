5c769b1192f97da318e13b0536db303b454c7e48
patrick <patrick.zierahn@student.kit.edu>
2024-04-16 12:57:39 +0200

    Refactor extract_feature_point_movement

--- respiratory_extraction/models/optical_flow/__init__.py
+++ respiratory_extraction/models/optical_flow/__init__.py
@@ -1,3 +1,4 @@
 from .feature_points import *
 from .pre_processing import *
 from .frequency_extraction import *
+from .feature_point_movement import *
---
+++ respiratory_extraction/models/optical_flow/feature_point_movement.py
@@ -0,0 +1,48 @@
+import cv2
+import numpy as np
+
+
+def extract_feature_point_movement(
+        frames: np.ndarray,
+        feature_points: np.ndarray,
+        win_size: (15, 15) | tuple[int, int],
+        max_level=int(2)) -> np.ndarray:
+    """
+    Extract the movement of the feature points in the frames using the Lucas-Kanade optical flow method.
+    :param frames:
+    :param feature_points:
+    :param win_size:
+    :param max_level:
+    :return:
+    """
+
+    lk_params = {
+        'winSize': win_size,
+        'maxLevel': max_level,
+    }
+    total_frame = len(frames)
+
+    # Store the feature points for each frame
+    feature_point_matrix = np.zeros((int(total_frame), feature_points.shape[0], 2))
+
+    # Store the feature points for the first frame
+    feature_point_matrix[0, :, 0] = feature_points[:, 0, 0].T
+    feature_point_matrix[0, :, 1] = feature_points[:, 0, 1].T
+
+    # Calculate the optical flow of the feature points for each frame
+    for inx in range(1, total_frame):
+        current_frame = frames[inx - 1]
+        next_frame = frames[inx]
+
+        new_positions, _, _ = cv2.calcOpticalFlowPyrLK(
+            current_frame,
+            next_frame,
+            feature_points,
+            None,
+            **lk_params)
+
+        feature_points = new_positions.reshape(-1, 1, 2)
+        feature_point_matrix[inx, :, 0] = feature_points[:, 0, 0].T
+        feature_point_matrix[inx, :, 1] = feature_points[:, 0, 1].T
+
+    return feature_point_matrix
