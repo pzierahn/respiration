4a9d0dec1c6698ccd49507b48ad43340a9c4e387
patrick <patrick.zierahn@student.kit.edu>
2024-04-17 13:33:13 +0200

    Refactor fft

--- respiratory_extraction/models/frequency_extraction/fft.py
+++ respiratory_extraction/models/frequency_extraction/fft.py
@@ -1,14 +1,18 @@
 import numpy as np
 
 
-def fft(data: np.ndarray, sample_rate: int, min_freq: float = 0, max_freq: float = float('inf')) -> float:
+def fft_spectrum(
+        data: np.ndarray,
+        sample_rate: int,
+        min_freq: float = 0,
+        max_freq: float = float('inf')) -> tuple[np.ndarray, np.ndarray]:
     """
-    Extract the predominant frequency from the data using the Fast Fourier Transform.
+    Extract the frequency spectrum from the data using the Fast Fourier Transform.
     :param data: Respiratory signal
     :param sample_rate: Sampling rate
     :param min_freq: minimum frequency
     :param max_freq: maximum frequency
-    :return: peak frequency
+    :return: Frequencies and FFT result
     """
 
     # Perform Fast Fourier Transform
@@ -21,6 +25,22 @@ def fft(data: np.ndarray, sample_rate: int, min_freq: float = 0, max_freq: float
     frequency_filter = (frequencies >= min_freq) & (frequencies <= max_freq)
     frequencies = frequencies[frequency_filter]
     fft_result = fft_result[frequency_filter]
+
+    return frequencies, fft_result
+
+
+def fft(data: np.ndarray, sample_rate: int, min_freq: float = 0, max_freq: float = float('inf')) -> float:
+    """
+    Extract the predominant frequency from the data using the Fast Fourier Transform.
+    :param data: Respiratory signal
+    :param sample_rate: Sampling rate
+    :param min_freq: minimum frequency
+    :param max_freq: maximum frequency
+    :return: peak frequency
+    """
+
+    # Perform Fast Fourier Transform
+    frequencies, fft_result = fft_spectrum(data, sample_rate, min_freq, max_freq)
 
     # Find the frequency corresponding to the maximum magnitude
     peak_freq = frequencies[np.argmax(np.abs(fft_result))]
