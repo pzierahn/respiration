8847857796a08b294cc3be43ee49bd478d22254f
patrick <patrick.zierahn@student.kit.edu>
2024-04-17 14:17:40 +0200

    Added prettify_json method

--- respiratory_extraction/utils/json.py
+++ respiratory_extraction/utils/json.py
@@ -1,7 +1,23 @@
 import json
 
 
+def prettify_json(data: any) -> str:
+    """
+    Prettify JSON data
+    :param data:
+    :return:
+    """
+    return json.dumps(data, indent=2, sort_keys=True, default=str)
+
+
 def write_json(filename: str, data: any):
+    """
+    Write JSON data to file
+    :param filename:
+    :param data:
+    :return:
+    """
+
     with open(filename, 'w') as file:
         json.dump(
             data,
