7f5f4b8063eb727e512ed8e49ee4f4d326aea5b5
patrick <patrick.zierahn@student.kit.edu>
2024-04-17 14:53:41 +0200

    Added pretty_print

--- respiratory_extraction/utils/json.py
+++ respiratory_extraction/utils/json.py
@@ -10,6 +10,15 @@ def prettify_json(data: any) -> str:
     return json.dumps(data, indent=2, sort_keys=True, default=str)
 
 
+def pretty_print(data: any):
+    """
+    Prettify JSON data and print it
+    :param data:
+    :return:
+    """
+    print(prettify_json(data))
+
+
 def write_json(filename: str, data: any):
     """
     Write JSON data to file
