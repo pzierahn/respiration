2f0f619461d6870c32543f7153a542a4830ca7e4
patrick <patrick.zierahn@student.kit.edu>
2024-04-19 17:29:19 +0200

    Added attention_mask.py

--- respiration/extractor/__init__.py
+++ respiration/extractor/__init__.py
@@ -1,2 +1,3 @@
+from .mtts_can import *
 from .pixel_intensity import *
 from .optical_flow import *
---
+++ respiration/extractor/mtts_can/attention_mask.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+
+
+class AttentionMask(tf.keras.layers.Layer):
+    def call(self, x):
+        xsum = tf.reduce_sum(x, axis=1, keepdims=True)
+        xsum = tf.reduce_sum(xsum, axis=2, keepdims=True)
+        return x / xsum * x.shape[1] * x.shape[2] * 0.5
+
+    def get_config(self):
+        config = super(AttentionMask, self).get_config()
+        return config
