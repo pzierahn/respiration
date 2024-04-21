854ec5144c695c9954306090d512697021e55d4e
patrick <patrick.zierahn@student.kit.edu>
2024-04-21 17:51:24 +0200

    Update video preprocessing

--- respiration/extractor/mtts_can/preprocess.py
+++ respiration/extractor/mtts_can/preprocess.py
@@ -1,36 +1,75 @@
 import cv2
 import numpy as np
+from skimage.util import img_as_float
 
 
-def preprocess_video_frames(frames: np.ndarray, target_dim=(36, 36)) -> tuple[np.ndarray, np.ndarray]:
+def preprocess_video_frames(frames: np.ndarray, dim=36) -> tuple[np.ndarray, np.ndarray]:
     """
     Preprocesses raw video frames by down sampling, normalizing (c(t + 1) − c(t))/(c(t + 1) + c(t)), and standardizing
     them.
     :param frames: Raw video frames
-    :param target_dim: Target dimensions for resizing
+    :param dim: Target dimensions for resizing
     :return: Resized and standardized frames
     """
+    total_frames = len(frames)
+    resized = np.zeros((total_frames, dim, dim, 3), dtype=np.float32)
 
-    # Resize all frames at once
-    resized_frames = np.array([
-        cv2.resize(frame, target_dim, interpolation=cv2.INTER_AREA) for frame in frames
-    ])
-    float_frames = resized_frames.astype(np.float32)
+    # Crop and resize each frame
+    for inx, img in enumerate(frames):
+        float_img = img_as_float(img)
+        resized_img = cv2.resize(
+            float_img,
+            (dim, dim),
+            interpolation=cv2.INTER_AREA)
+        resized_img = resized_img.astype('float32')
+        resized_img = cv2.cvtColor(resized_img, cv2.COLOR_BGR2RGB)
+        resized_img[resized_img > 1] = 1
+        resized_img[resized_img < (1 / 255)] = 1 / 255
+        resized[inx, :, :, :] = resized_img
 
-    # Calculate the nominator: c(t + 1) - c(t)
-    nom = float_frames[1:] - float_frames[:-1]
-    # Calculate the denominator: c(t + 1) + c(t)
-    denom = float_frames[1:] + float_frames[:-1] + 1e-8
+    # Normalize frames for motion branch
+    normalized = ((resized[1:] - resized[:-1]) /
+                  (resized[1:] + resized[:-1]))
+    normalized = normalized / np.std(normalized)
 
-    # Normalize frames
-    normalized_frames = nom / denom
+    resized = resized - np.mean(resized)
+    resized = resized / np.std(resized)
+    resized = resized[:total_frames - 1, :, :, :]
 
-    # Standardize frames using vectorized operations
-    mean_val = np.mean(normalized_frames, axis=(0, 1, 2), keepdims=True)
-    std_val = np.std(normalized_frames, axis=(0, 1, 2), keepdims=True)
-    standardized_frames = (normalized_frames - mean_val) / (std_val + 1e-5)
+    return resized, normalized
 
-    # Remove the first frame from the resized frames to match the number of frames
-    resized_frames = resized_frames[1:]
 
-    return resized_frames, standardized_frames
+def preprocess_frames_original(frames, dim=36):
+    total_frames = len(frames)
+    Xsub = np.zeros((total_frames, dim, dim, 3), dtype=np.float32)
+
+    # Assuming all frames have the same dimensions
+    height, width = frames[0].shape[:2]
+
+    # Crop and resize each frame
+    for i, img in enumerate(frames):
+        float_img = img_as_float(img[:, int(width / 2) - int(height / 2 + 1):int(height / 2) + int(width / 2), :])
+        vidLxL = cv2.resize(
+            float_img,
+            (dim, dim),
+            interpolation=cv2.INTER_AREA)
+        # vidLxL = cv2.rotate(vidLxL, cv2.ROTATE_90_CLOCKWISE)  # rotate 90 degree
+        vidLxL = cv2.cvtColor(vidLxL.astype('float32'), cv2.COLOR_BGR2RGB)
+        vidLxL[vidLxL > 1] = 1
+        vidLxL[vidLxL < (1 / 255)] = 1 / 255
+        Xsub[i, :, :, :] = vidLxL
+
+    # Normalize frames for motion branch
+    normalized_len = len(frames) - 1
+    dXsub = np.zeros((normalized_len, dim, dim, 3), dtype=np.float32)
+    for j in range(normalized_len - 1):
+        # c(t + 1) - c(t) / c(t + 1) + c(t)
+        dXsub[j, :, :, :] = (Xsub[j + 1, :, :, :] - Xsub[j, :, :, :]) / (Xsub[j + 1, :, :, :] + Xsub[j, :, :, :])
+    dXsub = dXsub / np.std(dXsub)
+
+    # Normalize raw frames for appearance branch
+    Xsub = Xsub - np.mean(Xsub)
+    Xsub = Xsub / np.std(Xsub)
+    Xsub = Xsub[:total_frames - 1, :, :, :]
+
+    return Xsub, dXsub
