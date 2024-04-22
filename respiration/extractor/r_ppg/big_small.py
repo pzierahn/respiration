bd43434907773eb465b1b4ffe7051fad89e04651
patrick <patrick.zierahn@student.kit.edu>
2024-04-22 16:14:32 +0200

    Added deep_phys and big_small

--- respiration/extractor/r_ppg/big_small.py
+++ respiration/extractor/r_ppg/big_small.py
@@ -28,12 +28,11 @@ class WTSM(nn.Module):
         fold = c // self.fold_div
         out = torch.zeros_like(x)
         out[:, :-1, :fold] = x[:, 1:, :fold]  # shift left
-        out[:, -1, :fold] = x[:, 0, :fold] # wrap left
+        out[:, -1, :fold] = x[:, 0, :fold]  # wrap left
         out[:, 1:, fold: 2 * fold] = x[:, :-1, fold: 2 * fold]  # shift right
         out[:, 0, fold: 2 * fold] = x[:, -1, fold: 2 * fold]  # wrap right
         out[:, :, 2 * fold:] = x[:, :, 2 * fold:]  # no shift for final fold
         return out.view(nt, c, h, w)
-
 
 
 #######################################################################################
@@ -41,10 +40,22 @@ class WTSM(nn.Module):
 #######################################################################################
 class BigSmall(nn.Module):
 
-    def __init__(self, in_channels=3, nb_filters1=32, nb_filters2=64, kernel_size=3, 
-                 dropout_rate1=0.25, dropout_rate2=0.5, dropout_rate3=0.5, pool_size1=(2, 2), pool_size2=(4,4),
-                 nb_dense=128, out_size_bvp=1, out_size_resp=1, out_size_au=12, n_segment=3):
-
+    def __init__(
+            self,
+            in_channels=3,
+            nb_filters1=32,
+            nb_filters2=64,
+            kernel_size=3,
+            dropout_rate1=0.25,
+            dropout_rate2=0.5,
+            dropout_rate3=0.5,
+            pool_size1=(2, 2),
+            pool_size2=(4, 4),
+            nb_dense=128,
+            out_size_bvp=1,
+            out_size_resp=1,
+            out_size_au=12,
+            n_segment=3):
         super(BigSmall, self).__init__()
 
         self.in_channels = in_channels
@@ -65,12 +76,22 @@ class BigSmall(nn.Module):
         self.n_segment = n_segment
 
         # Big Convolutional Layers
-        self.big_conv1 = nn.Conv2d(self.in_channels, self.nb_filters1, kernel_size=self.kernel_size, padding=(1, 1), bias=True)
-        self.big_conv2 = nn.Conv2d(self.nb_filters1, self.nb_filters1, kernel_size=self.kernel_size, padding=(1, 1), bias=True)
-        self.big_conv3 = nn.Conv2d(self.nb_filters1, self.nb_filters1, kernel_size=self.kernel_size, padding=(1, 1), bias=True)
-        self.big_conv4 = nn.Conv2d(self.nb_filters1, self.nb_filters2, kernel_size=self.kernel_size, padding=(1, 1), bias=True)
-        self.big_conv5 = nn.Conv2d(self.nb_filters2, self.nb_filters2, kernel_size=self.kernel_size, padding=(1, 1), bias=True)
-        self.big_conv6 = nn.Conv2d(self.nb_filters2, self.nb_filters2, kernel_size=self.kernel_size, padding=(1, 1), bias=True)
+        self.big_conv1 = nn.Conv2d(
+            self.in_channels,
+            self.nb_filters1,
+            kernel_size=self.kernel_size,
+            padding=(1, 1),
+            bias=True)
+        self.big_conv2 = nn.Conv2d(self.nb_filters1, self.nb_filters1, kernel_size=self.kernel_size, padding=(1, 1),
+                                   bias=True)
+        self.big_conv3 = nn.Conv2d(self.nb_filters1, self.nb_filters1, kernel_size=self.kernel_size, padding=(1, 1),
+                                   bias=True)
+        self.big_conv4 = nn.Conv2d(self.nb_filters1, self.nb_filters2, kernel_size=self.kernel_size, padding=(1, 1),
+                                   bias=True)
+        self.big_conv5 = nn.Conv2d(self.nb_filters2, self.nb_filters2, kernel_size=self.kernel_size, padding=(1, 1),
+                                   bias=True)
+        self.big_conv6 = nn.Conv2d(self.nb_filters2, self.nb_filters2, kernel_size=self.kernel_size, padding=(1, 1),
+                                   bias=True)
 
         # Big Avg Pooling / Dropout Layers
         self.big_avg_pooling1 = nn.AvgPool2d(self.pool_size1)
@@ -85,12 +106,16 @@ class BigSmall(nn.Module):
         self.TSM_2 = WTSM(n_segment=self.n_segment)
         self.TSM_3 = WTSM(n_segment=self.n_segment)
         self.TSM_4 = WTSM(n_segment=self.n_segment)
-        
+
         # Small Convolutional Layers
-        self.small_conv1 = nn.Conv2d(self.in_channels, self.nb_filters1, kernel_size=self.kernel_size, padding=(1,1), bias=True)
-        self.small_conv2 = nn.Conv2d(self.nb_filters1, self.nb_filters1, kernel_size=self.kernel_size, padding=(1,1), bias=True)
-        self.small_conv3 = nn.Conv2d(self.nb_filters1, self.nb_filters1, kernel_size=self.kernel_size, padding=(1,1), bias=True)
-        self.small_conv4 = nn.Conv2d(self.nb_filters1, self.nb_filters2, kernel_size=self.kernel_size, padding=(1,1), bias=True)
+        self.small_conv1 = nn.Conv2d(self.in_channels, self.nb_filters1, kernel_size=self.kernel_size, padding=(1, 1),
+                                     bias=True)
+        self.small_conv2 = nn.Conv2d(self.nb_filters1, self.nb_filters1, kernel_size=self.kernel_size, padding=(1, 1),
+                                     bias=True)
+        self.small_conv3 = nn.Conv2d(self.nb_filters1, self.nb_filters1, kernel_size=self.kernel_size, padding=(1, 1),
+                                     bias=True)
+        self.small_conv4 = nn.Conv2d(self.nb_filters1, self.nb_filters2, kernel_size=self.kernel_size, padding=(1, 1),
+                                     bias=True)
 
         # AU Fully Connected Layers 
         self.au_fc1 = nn.Linear(5184, self.nb_dense, bias=True)
@@ -104,19 +129,16 @@ class BigSmall(nn.Module):
         self.resp_fc1 = nn.Linear(5184, self.nb_dense, bias=True)
         self.resp_fc2 = nn.Linear(self.nb_dense, self.out_size_resp, bias=True)
 
-
     def forward(self, inputs, params=None):
-
-        big_input = inputs[0] # big res 
-        small_input = inputs[1] # small res
+        big_input = inputs[0]  # big res
+        small_input = inputs[1]  # small res
 
         # reshape Big 
         nt, c, h, w = big_input.size()
         n_batch = nt // self.n_segment
         big_input = big_input.view(n_batch, self.n_segment, c, h, w)
-        big_input = torch.moveaxis(big_input, 1, 2) # color channel to idx 1, sequence channel to idx 2
-        big_input = big_input[:, :, 0, :, :] # use only first frame in sequences 
-
+        big_input = torch.moveaxis(big_input, 1, 2)  # color channel to idx 1, sequence channel to idx 2
+        big_input = big_input[:, :, 0, :, :]  # use only first frame in sequences
 
         # Big Conv block 1
         b1 = nn.functional.relu(self.big_conv1(big_input))
@@ -137,10 +159,10 @@ class BigSmall(nn.Module):
         b12 = self.big_dropout3(b11)
 
         # Reformat Big Shape For Concat w/ Small Branch
-        b13 = torch.stack((b12, b12, b12), 2) #TODO: this is hardcoded for num_segs = 3: change this...
+        b13 = torch.stack((b12, b12, b12), 2)  # TODO: this is hardcoded for num_segs = 3: change this...
         b14 = torch.moveaxis(b13, 1, 2)
         bN, bD, bC, bH, bW = b14.size()
-        b15 = b14.reshape(int(bN*bD), bC, bH, bW)
+        b15 = b14.reshape(int(bN * bD), bC, bH, bW)
 
         # Small Conv block 1
         s1 = self.TSM_1(small_input)
@@ -155,7 +177,7 @@ class BigSmall(nn.Module):
         s8 = nn.functional.relu(self.small_conv4(s7))
 
         # Shared Layers
-        concat = b15 + s8 # sum layers
+        concat = b15 + s8  # sum layers
 
         # share1 = concat.view(concat.size(0), -1) # flatten entire tensors
         share1 = concat.reshape(concat.size(0), -1)
@@ -173,5 +195,3 @@ class BigSmall(nn.Module):
         resp_out = self.resp_fc2(respfc1)
 
         return au_out, bvp_out, resp_out
-
-
