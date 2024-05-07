3ad2324e64309619e28c1901843a74d3d62d9a00
patrick <patrick.zierahn@student.kit.edu>
2024-05-07 16:19:58 +0200

    Update comment

--- respiration/extractor/optical_flow_raft/preprocessing.py
+++ respiration/extractor/optical_flow_raft/preprocessing.py
@@ -4,6 +4,13 @@ import torchvision.transforms as transform
 
 
 def preprocess(frames: np.ndarray, device: torch.device) -> torch.Tensor:
+    """
+    Convert frames to torch.Tensor and apply normalization.
+    :param frames: (N, H, W, C) numpy array
+    :param device: torch.device
+    :return: (N, C, H, W) torch.Tensor
+    """
+    # Permute from (N, H, W, C) to (N, C, H, W)
     frames = torch.from_numpy(frames).permute(0, 3, 1, 2)
 
     transforms = transform.Compose(
