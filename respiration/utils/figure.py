0f610139c07269a45738385b3eca89460250c9df
patrick <patrick.zierahn@student.kit.edu>
2024-05-08 21:42:06 +0200

    Refactor to savefig

--- respiration/utils/figure.py
+++ respiration/utils/figure.py
@@ -2,18 +2,16 @@ import os
 import matplotlib.pyplot as plt
 
 
-def store_figure(fig: plt.Figure, dirname: str, name: str) -> None:
+def savefig(fig: plt.Figure, dir: str, filename: str) -> None:
     """
     Store a figure in the reports/figures directory.
     :param fig: The figure to store.
-    :param dirname: The directory name to store the figure in.
-    :param name: The name of the figure.
+    :param dir: The directory to store the figure in.
+    :param filename: The filename of the figure.
     """
-    path = os.path.join(os.getcwd(), '..', '..', 'outputs', 'figures', dirname)
-    os.makedirs(path, exist_ok=True)
 
-    path_png = os.path.join(path, f'{name}.png')
+    path_png = os.path.join(dir, f'{filename}.png')
     fig.savefig(path_png, format='png')
 
-    path_svg = os.path.join(path, f'{name}.svg')
+    path_svg = os.path.join(dir, f'{filename}.svg')
     fig.savefig(path_svg, format='svg')
