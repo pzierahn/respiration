6b709692f8537101f6fb0e71b6d5eeaded83e190
patrick <patrick.zierahn@student.kit.edu>
2024-06-08 11:42:03 +0200

    Refactor preprocess_video_frames to utils

--- respiration/extractor/mtts_can/preprocess.py
+++ respiration/extractor/mtts_can/preprocess.py
@@ -2,40 +2,14 @@ import cv2
 import numpy as np
 from skimage.util import img_as_float
 
+import respiration.utils as utils
+
 
 def preprocess_video_frames(frames: np.ndarray, dim=36) -> tuple[np.ndarray, np.ndarray]:
     """
-    Preprocesses raw video frames by down sampling, normalizing (c(t + 1) − c(t))/(c(t + 1) + c(t)), and standardizing
-    them.
-    :param frames: Raw video frames
-    :param dim: Target dimensions for resizing
-    :return: Resized and standardized frames
+    Deprecated: Use respiration.utils.preprocess_frames instead.
     """
-    total_frames = len(frames)
-    resized = np.zeros((total_frames, dim, dim, 3), dtype=np.float32)
-
-    # Crop and resize each frame
-    for inx, img in enumerate(frames):
-        float_img = img_as_float(img)
-        resized_img = cv2.resize(
-            float_img,
-            (dim, dim),
-            interpolation=cv2.INTER_AREA)
-        resized_img = resized_img.astype('float32')
-        resized_img[resized_img > 1] = 1
-        resized_img[resized_img < (1 / 255)] = 1 / 255
-        resized[inx, :, :, :] = resized_img
-
-    # Normalize frames for motion branch
-    normalized = ((resized[1:] - resized[:-1]) /
-                  (resized[1:] + resized[:-1]))
-    normalized = normalized / np.std(normalized)
-
-    resized = resized - np.mean(resized)
-    resized = resized / np.std(resized)
-    resized = resized[:total_frames - 1, :, :, :]
-
-    return resized, normalized
+    return utils.preprocess_video_frames(frames, dim)
 
 
 def preprocess_frames_original(frames, dim=36):
--- respiration/utils/__init__.py
+++ respiration/utils/__init__.py
@@ -1,6 +1,7 @@
 from .figure import *
 from .json import *
 from .path import *
+from .temporal_shifting import *
 from .torch import *
 from .unisens import *
 from .video import *
