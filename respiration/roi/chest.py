9368df2e39ec452503babd1e32810ed6c557b717
patrick <patrick.zierahn@student.kit.edu>
2024-07-13 10:06:01 +0200

    Added face parameters to detect_chest function

--- respiration/roi/chest.py
+++ respiration/roi/chest.py
@@ -28,16 +28,24 @@ def chest_from_face(face: Sequence[int],
     return chest_x, chest_y, chest_w, chest_h
 
 
-def detect_chest(frame: np.ndarray, scale_w: float = 0, scale_h: float = 0) -> tuple[int, int, int, int]:
+def detect_chest(
+        frame: np.ndarray,
+        scale_w: float = 0,
+        scale_h: float = 0,
+        face_scale_factor: float = 1.3,
+        face_min_neighbors: int = 5,
+) -> tuple[int, int, int, int]:
     """
     Calculate the chest region of interest (ROI) based on the face detected in frame
     :param frame: The frame to detect the chest from
     :param scale_w: scale factor for the ROI width
     :param scale_h: scale factor for the ROI height
+    :param face_scale_factor: scaleFactor parameter for detectMultiScale
+    :param face_min_neighbors: minNeighbors parameter for detectMultiScale
     :return: tuple of the chest region (x, y, w, h)
     """
 
-    faces = detect_faces(frame)
+    faces = detect_faces(frame, face_scale_factor, face_min_neighbors)
 
     assert len(faces) == 1, f"Expected 1 face, but got {len(faces)}"
 
