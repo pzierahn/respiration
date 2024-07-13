78864947a3f383f4b9bfb4d0bbd4a8d2ca7e818b
patrick <patrick.zierahn@student.kit.edu>
2024-07-13 11:34:59 +0200

    Remove standard_processing function

--- respiration/preprocessing/pre_processing.py
+++ respiration/preprocessing/pre_processing.py
@@ -32,34 +32,3 @@ def normalize_signal(time_series: np.ndarray) -> np.ndarray:
     :return:
     """
     return (time_series - np.mean(time_series)) / np.std(time_series)
-
-
-def standard_processing(
-        respiratory_signal: np.ndarray,
-        fps: float = 30.0,
-        lowpass: float = 0.08,
-        highpass: float = 0.6,
-        order: int = 3,
-) -> np.ndarray:
-    """
-    Apply the standard processing to the signal.
-    :param respiratory_signal: The signal data.
-    :param fps: The frames per second.
-    :param lowpass: The lowpass frequency in Hz.
-    :param highpass: The highpass frequency in Hz.
-    :param order: The order of the filter.
-    :return:
-    """
-    detrend = signal.detrend(respiratory_signal)
-
-    # Apply a Butterworth filter
-    filtered_signal = butterworth_filter(
-        detrend,
-        fps,
-        lowpass,
-        highpass,
-        order
-    )
-
-    # Normalize the signal
-    return normalize_signal(filtered_signal)
